83c94425465243c6bfd771e222ee27d55d1f7a46
kauazinobrabo45 <128061954+kauazinobrabo45@users.noreply.github.com>
2024-10-24 12:16:39 -0300

    df

--- InformaçoesGlobais.js
+++ InformaçoesGlobais.js
@@ -1,3 +1,3 @@
-const url ='https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
 
 console.log(url);
